c6e456f6ab4e8c9c92bb8184e3604ddece2887d0
Antonio <antonio.saenz.c@uni.pe>
2026-05-23 17:01:32 -0500

    Prueba completa

--- Topologias/scripts/configs/comandos_validacion_enrutamiento.py
+++ Topologias/scripts/configs/comandos_validacion_enrutamiento.py
@@ -1,14 +1,12 @@
 COMANDOS_SUP = {
     
     "CPE-HQ": [
-        "show version",
         "show ip interface brief",
-        "show running-config",
         "show crypto isakmp sa",
         "show crypto ipsec sa",
         "show ip ospf neighbor",
         "show ip ospf interface Tunnel1",
-        "show ip ospf database router" ,
+        "show ip ospf database router",
         "show ip route ospf",
         "show ip cef 192.168.25.2",
         "show ip cef exact-route 172.16.10.1 192.168.25.2",
@@ -16,27 +14,20 @@ COMANDOS_SUP = {
         "traceroute 192.168.25.2",
         "ping 172.16.10.4",
         "traceroute 172.16.10.4",
-        "ping 180.0.0.1",
-        "traceroute 180.0.0.1",
     ],
     
     "M3": [
         "show ip interface brief",
-        "show interface Ethernet1/0",
         "show ip ospf neighbor",
         "show ip ospf database",
         "show ip route static",
-        "show ip route 180.0.0.0"
+        "show ip route 180.0.0.0",
         "show ip cef 192.168.25.2",
         "ping 10.0.0.73",
-        "ping 192.168.25.2",
-        "traceroute 192.168.25.2"
     ],
     
     "CPE-BRANCH2": [
-        "show version",
         "show ip interface brief",
-        "show running-config",
         "show crypto isakmp sa",
         "show crypto ipsec sa",
         "show ip ospf neighbor",
@@ -44,16 +35,11 @@ COMANDOS_SUP = {
         "show ip ospf database router",
         "show ip route ospf",
         "show ip route",
-        "show ip nat translations",####
         "show ip nat statistics",
         "show ip interface brief",
         "show run interface Ethernet0/2.25",
         "ping 172.16.10.1",
         "traceroute 172.16.10.1",
-        "ping 172.16.20.1",
-        "traceroute 172.16.20.1",
-        "ping 172.20.20.5",
-        "traceroute 172.20.20.5",
     ],
     
     "SW2-R-PISO1": [
